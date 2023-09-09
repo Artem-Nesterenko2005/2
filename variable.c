8913fd5148bf131b24c66a7391b7bdbba0448580
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-10 02:15:50 +0500

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int variable_1, variable_2 = 0;
+	printf("Enter the first integer variable ");
+	scanf_s("%d", &variable_1);
+	printf("Enter the second integer variable ");
+	scanf_s("%d", &variable_2);
+	if (variable_1 > variable_2)
+	{
+	   	variable_1 = variable_1 + variable_2;
+		variable_2 = variable_1 - variable_2;
+		variable_1 = variable_1 - variable_2;
+	}
+	else
+	{
+		variable_2 = variable_1 + variable_2;
+		variable_1 = variable_2 - variable_1;
+		variable_2 = variable_2 - variable_1;
+	}
+	printf("First variable = %d\n", variable_1);
+	printf("Second variable = %d", variable_2);
+}
+
+
